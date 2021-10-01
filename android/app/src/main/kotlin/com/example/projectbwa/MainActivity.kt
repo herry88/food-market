ff6469abc27d8d0e0474d968cadd41b23306855a
herry88 <herry081288@gmail.com>
2021-10-01 14:08:58 +0700

    flutter init

---
+++ android/app/src/main/kotlin/com/example/projectbwa/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.projectbwa
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/pages/general_page.dart
@@ -0,0 +1,23 @@
+part of 'pages.dart';
+
+class GeneralPage extends StatelessWidget {
+  // final String title;
+  // final String subtitle;
+  // final Function onBackButtonOnPressed;
+  // final Widget child;
+  // GeneralPage({
+  //   this.title = "Title",
+  //   required this.subtitle,
+  //   required this.onBackButtonOnPressed,
+  //   required this.child,
+  // });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        color: Colors.white,
+      ),
+    );
+  }
+}
