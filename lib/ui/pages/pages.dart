ab1225f1c0a9e07c606c640485db361848211245
herry88 <herry081288@gmail.com>
2021-10-11 14:26:17 +0700

    signInpage

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:projectbwa/shared/shared.dart';
 import 'package:supercharged/supercharged.dart';
+
+//ui
 part 'general_page.dart';
+part 'sign_in_page.dart';
