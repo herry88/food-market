4cc72662fb5e1c25b437256903e31f9b1e9b5c12
herry88 <herry081288@gmail.com>
2021-10-11 14:58:57 +0700

    AddWidgetAndpackage

---
+++ lib/ui/widgets/food_cart.dart
@@ -0,0 +1,13 @@
+part of 'widgets.dart';
+
+class FoodCard extends StatelessWidget {
+  const FoodCard({ Key key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 200.0,
+      height: 210,
+    );
+  }
+}
