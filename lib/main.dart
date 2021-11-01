d2886dac68ecf2b4e52558d3f76cce61277e9ace
herry88 <herry081288@gmail.com>
2021-11-01 14:13:34 +0700

    ui payment

--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false, home: FoodDetailsPage());
+      debugShowCheckedModeBanner: false,
+      home: PaymentPage(),
+    );
   }
 }
