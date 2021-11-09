20c157f0d8f3dd7055f735cdd06b0a381db2ed43
herry88 <herry081288@gmail.com>
2021-11-09 11:54:17 +0700

    yoo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
 import 'ui/pages/pages.dart';
 
 void main() {
@@ -10,9 +11,9 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      home: PaymentPage(),
+      home: MainPage(),
     );
   }
 }
