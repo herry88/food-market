6abdf3133b7168e21b8e474665b616f26baa1e75
herry88 <herry081288@gmail.com>
2021-11-24 10:07:17 +0700

    yo

---
+++ lib/ui/pages/success_sign_up.dart
@@ -0,0 +1,12 @@
+part of 'pages.dart';
+
+class SuccessSignUpPage extends StatelessWidget {
+  const SuccessSignUpPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+    );
+  }
+}
